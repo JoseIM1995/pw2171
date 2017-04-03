7cc12bcf4d8a3fded480c82eeb1f59039576d37d
pebble.ad@hotmail.com <pebble.ad@hotmail.com>
2017-04-03 15:03:53 -0600

    Ejemplo01 JavaScript

---
+++ javascript
@@ -0,0 +1,8 @@
+JavaScript: (lenguaje de programación)
+*Se integra con HTML para validaciones.
+*LiveScript (menos poderoso).
+*Sintáxis Java. (if, for, while, etc.)
+*Punto y coma es opcional (recomienda).
+*Elimina el ocultamiento
+(se mira el código del lado del cliente).
+*Existe JavaScript del lado del servidor en distintas aplicaciones.
---
+++ js/sumas.js
@@ -0,0 +1,16 @@
+	function suma3{
+		var n1 = document.getElementById("num1")).value)
+		var n2 = document.getElementById("num1")).value)
+		alert(ParseInt(n1)+parseInt(n2));
+	}
+
+	function texto(){
+		alert(document.getElementById("texto1")).value)
+	}
+
+	function suma(a,b){
+			return a+b;
+		}
+		var suma2 = function(a,b){
+			return a+b;
+		}
