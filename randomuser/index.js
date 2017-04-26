f785813971210f579fbfd1251d94de5c00e967f6
pebble.ad@hotmail.com <pebble.ad@hotmail.com>
2017-04-26 14:59:52 -0600

    Usuarios Random

--- randomuser/index.js
+++ randomuser/index.js
@@ -6,9 +6,18 @@ function datosRandom(){
 	  url: 'https://randomuser.me/api/',
 	  dataType: 'json',
 	  success: function(data) {
+	  	$("#txtGender").html(data.results[0].gender)
 	    $("#txtNombre").html(data.results[0].name.first+" "+
 	    					 data.results[0].name.last)
+	    $("#txtTitle").html(data.results[0].name.title)
 	    $("#imgFoto").attr("src",data.results[0].picture.large)
+	    $("#txtStreet").html(data.results[0].location.street)
+	    $("#txtCity").html(data.results[0].location.city)
+	    $("#txtState").html(data.results[0].location.state)
+	    $("#txtPostCode").html(data.results[0].location.postcode)
+	    $("#txtEmail").html(data.results[0].email)
+	    $("#txtUserName").html(data.results[0].login.username)
+	    $("#txtPassword").html(data.results[0].login.password)
 	  },
 	  error(a,b,c){
 	  	alert("Sin internet o sin servidor")
